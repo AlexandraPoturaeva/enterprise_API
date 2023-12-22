c30d3d06fa863c2603656643551187779aedb272
AleksandraPoturaeva <av-poturaeva@yandex.ru>
2023-12-22 16:39:31 +0300

    install dependencies for API creation

--- enterprise_API/settings.py
+++ enterprise_API/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'enterprise_structure',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'enterprise_structure',
+    'rest_framework',
+    'drf_yasg',
+    'mptt',
 ]
 
 MIDDLEWARE = [
