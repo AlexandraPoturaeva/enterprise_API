8eb73898ac2f609ab339b9234cc5787c99c72871
AleksandraPoturaeva <av-poturaeva@yandex.ru>
2023-12-24 17:29:06 +0300

    create models Employee, Rule and Position

--- enterprise_structure/models.py
+++ enterprise_structure/models.py
@@ -10,6 +10,21 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+class Rule(TimeStampedModel):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f'id {self.pk}: {self.title}'
+
+
+class Position(TimeStampedModel):
+    title = models.CharField(max_length=100, unique=True)
+    rules = models.ManyToManyField(Rule, blank=True)
+
+    def __str__(self):
+        return f'id {self.pk}: {self.title}'
+
+
 class Division(MPTTModel, TimeStampedModel):
     title = models.CharField(max_length=100)
     parent_division = TreeForeignKey(
@@ -27,5 +42,17 @@ class Division(MPTTModel, TimeStampedModel):
     class Meta:
         ordering = ['created_at']
 
+
+class Employee(TimeStampedModel):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    birth_date = models.DateField(max_length=50)
+    division = models.ForeignKey(Division, models.PROTECT)
+    positions = models.ManyToManyField(Position)
+
+    class Meta:
+        ordering = ['surname']
+        unique_together = ['name', 'surname', 'birth_date']
+
     def __str__(self):
-        return self.title
+        return f'{self.name} {self.surname}'
